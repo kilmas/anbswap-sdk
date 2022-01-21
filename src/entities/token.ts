5d594da07bd65c48b52dc5bad0e7cd79926ef605
kilmas <kilmas@qq.com>
2022-01-21 15:34:34 +0800

    fix: weth

--- src/entities/token.ts
+++ src/entities/token.ts
@@ -61,11 +61,11 @@ export function currencyEquals(currencyA: Currency, currencyB: Currency): boolea
 export const WETH = {
   [ChainId.MAINNET]: new Token(ChainId.MAINNET, '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2', 18, 'WETH', 'Wrapped Ether'),
   [ChainId.ROPSTEN]: new Token(ChainId.ROPSTEN, '0xc778417E063141139Fce010982780140Aa0cD5Ab', 18, 'WETH', 'Wrapped Ether'),
-  [ChainId.RINKEBY]: new Token(ChainId.RINKEBY, '0xc778417E063141139Fce010982780140Aa0cD5Ab', 18, 'WETH', 'Wrapped Ether'),
+  [ChainId.RINKEBY]: new Token(ChainId.RINKEBY, '0x6D510a4C1c7052615BDf04849B6d6d3B0d15A100', 18, 'WETH', 'Wrapped Ether'),
   [ChainId.GÖRLI]: new Token(ChainId.GÖRLI, '0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6', 18, 'WETH', 'Wrapped Ether'),
   [ChainId.KOVAN]: new Token(ChainId.KOVAN, '0xd0A1E359811322d97991E03f863a0C30C2cF029C', 18, 'WETH', 'Wrapped Ether'),
   [ChainId.HECOMAIN]: new Token(ChainId.HECOMAIN, '0x5545153ccfca01fbd7dd11c0b23ba694d9509a6f', 18, 'WHT', 'Wrapped HT'),
-  [ChainId.HECOTEST]: new Token(ChainId.HECOTEST, '0x50b29a42c9e4d0f940292c386207f492458ee358', 18, 'WHT', 'Wrapped HT'),
+  [ChainId.HECOTEST]: new Token(ChainId.HECOTEST, '0x7aF326B6351C8A9b8fb8CD205CBe11d4Ac5FA836', 18, 'WHT', 'Wrapped HT'),
   [ChainId.BIANMAIN]: new Token(ChainId.BIANMAIN, '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c', 18, 'WBNB', 'Wrapped BNB'),
   [ChainId.BIANTEST]: new Token(ChainId.BIANTEST, '0x094616F0BdFB0b526bD735Bf66Eca0Ad254ca81F', 18, 'WBNB', 'Wrapped BNB'),
   [ChainId.OKTEST]: new Token(ChainId.OKTEST, '0xB13991B6253172118b962045906b27ce73C21ecD', 18, 'WOKT', 'Wrapped OKT')
